a41f87b96a6e87a84788cc565d6dad45f508920a
Prince Dixit <dixitprince895@gmail.com>
2026-02-24 15:42:33 +0530

    Initialize telecom data processing script
    Load telecom data, clean, and sort by tenure.

---
+++ Project1.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+
+telecom = pd.read_csv("/Users/ayush/Desktop/project/telecom_churn - Sheet1.csv")
+
+telecom.head()
+telecom.shape
+telecom.info()
+telecom.describe()
+telecom.isnull().sum()
+telecom.dropna(inplace=True)
+telecom.rename(columns={"Customer_ID": "Cust_ID"}, inplace=True)
+print(telecom)
+
+telecom.fillna(telecom.mean(numeric_only=True), inplace=True)
+print(telecom)
+
+sorted_customers = telecom.sort_values(by="Tenure_Months")
+print(sorted_customers)
